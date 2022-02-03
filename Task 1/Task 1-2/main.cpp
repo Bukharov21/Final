f728e33625d79d753201515f73bba7ba2c6d0af7
Сергей Бухаров <91834552+Bukharov21@users.noreply.github.com>
2022-02-03 21:18:14 +0300

    Update main.cpp

--- Task 1/Task 1-2/main.cpp
+++ Task 1/Task 1-2/main.cpp
@@ -1,12 +1,42 @@
-#define _USE_MATH_DEFINES
-#include <cmath>
 #include <iostream>
 
 using namespace std;
 
 /**
-* \brief Точка входа в программу
-* \return 0  в случае успеха
+*\brief Функция вычисляет сумму
+*\param x первое число
+*\param y второе число
+*\return сумму
+*/
+double getSum (const double x, const double y);
+
+/**
+*\brief Функция вычисляет разность
+*\param x первое число
+*\param y второе число
+*\return сумму
+*/
+double getDifference (const double x, const double y);
+
+/**
+*\brief Функция вычисляет произведение
+*\param x первое число
+*\param y второе число
+*\return сумму
+*/
+double getProduct (const double x, const double y);
+
+/**
+*\brief Функция вычисляет частное
+*\param x первое число
+*\param y второе число
+*\return сумму
+*/
+double getQuotient (const double x, const double y);
+
+/**
+*\brief Точка входа в программу
+*\return 0 в случае успеха
 */
 int main()
 {
@@ -18,10 +48,29 @@ int main()
     cout << " Введите второе число: " << "\n" << " y = ";
     cin >> y;
 
-    cout << " x + y = " << x + y << endl;
-    cout << " x - y = " << x - y << endl;
-    cout << " x * y = " << x * y << endl;
-    cout << " x : y = " << x / y << endl;
-
+    cout << " x + y = " << getSum(x, y) << endl;
+    cout << " x - y = " << getDifference(x, y) << endl;
+    cout << " x * y = " << getProduct(x, y) << endl;
+    cout << " x : y = " << getQuotient(x, y) << endl;
     return 0;
+}
+
+double getSum(const double x, const double y)
+{
+	return x + y;
+}
+
+double getDifference(const double x, const double y)
+{
+	return x - y;
+}
+
+double getProduct(const double x, const double y)
+{
+	return x * y;
+}
+
+double getQuotient(const double x, const double y)
+{
+	return x / y;
 }
