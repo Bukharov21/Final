7887d8790f91df2e374c58276ab56cde58be9305
Сергей Бухаров <91834552+Bukharov21@users.noreply.github.com>
2022-02-17 11:37:58 +0300

    Update Source.cpp

--- Task 3/Task 3-1/Source.cpp
+++ Task 3/Task 3-1/Source.cpp
@@ -2,30 +2,32 @@
 #include <cmath>
 #include <iomanip>
 
+using namespace std;
+
 /**
-*\brief Расчет функции в точке x
-*\param x входной параметр - точка x
-*\return Значение функции
+*\ brief Расчет функции в точке x.
+*\ param x входной параметр - точка x.
+*\ return Значение функции.
 */
 double Calculation(const double x);
 
 /**
-*\brief точка входа в программу
-*\return код ошибки (0 - успех)
+*\ brief точка входа в программу.
+*\ return код ошибки (0 - успех).
 */
 int main()
 {
-	const auto LeftBound = 1.0;
-	const auto RightBound = 3.0;
-	const auto Step = 0.2;
+	const auto leftBound = 1.0;
+	const auto rightBound = 3.0;
+	const auto step = 0.2;
 
-	auto x = LeftBound;
-	std::cout << std::setw(9) << "x" << std::setw(16) << "y\n";
-	while ((x < RightBound) || (abs(x - RightBound) < Step))
+	auto x = leftBound;
+	cout << setw(9) << "x" << setw(16) << "y\n";
+	while ((x < rightBound) || (abs(x - rightBound) < step))
 	{
 		const auto y = Calculation(x);
-		std::cout << std::setw(10) << std::setprecision(2) << x << std::setw(15) << std::setprecision(5) << y << "\n";
-		x += Step;
+		cout << setw(10) << setprecision(2) << x << setw(15) << setprecision(5) << y << "\n";
+		x += step;
 	}
 	return 0;
 }
