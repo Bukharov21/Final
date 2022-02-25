8e80b64cdccadf37564f8bde304570946b462b73
Сергей Бухаров <91834552+Bukharov21@users.noreply.github.com>
2022-02-25 19:19:10 +0300

    Update Source.cpp

--- Task 3/Task 3-1/Source.cpp
+++ Task 3/Task 3-1/Source.cpp
@@ -12,6 +12,12 @@ using namespace std;
 double Calculation(const double x);
 
 /**
+*\ brief Проверка на существование функции в x. 
+*\ brief param x заданная точка. 
+*\ return true, если значение в x существует. 
+bool isExists(const double x);
+
+/**
 *\ brief точка входа в программу.
 *\ return код ошибки (0 - успех).
 */
@@ -25,9 +31,16 @@ int main()
 	cout << setw(9) << "x" << setw(16) << "y\n";
 	while ((x < rightBound) || (abs(x - rightBound) < step))
 	{
-		const auto y = Calculation(x);
-		cout << setw(10) << setprecision(2) << x << setw(15) << setprecision(5) << y << "\n";
-		x += step;
+		if(isExists(x)) 
+		{
+			const auto y = Calculation(x);
+			cout << setw(10) << setprecision(2) << x << setw(15) << setprecision(5) << y << "\n";
+		else
+		{
+			cout << "x= " << x << "y не существует\n";
+		} 
+		} 
+		x += step; 
 	}
 	return 0;
 }
@@ -35,4 +48,9 @@ int main()
 double Calculation (const double x)
 {
 	return sin(log(x)) - cos(log(x)) + 2 * (log(x));
+}
+
+bool isExists(const double x)
+{
+	return x<=0;
 }
